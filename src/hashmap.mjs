26a4f4734adc5cb89f7e503b18b008e8db9986f6
KVG24 <141866132+KVG24@users.noreply.github.com>
2024-12-28 10:08:49 +0000

    Finish project

--- src/hashmap.mjs
+++ src/hashmap.mjs
@@ -1,6 +1,7 @@
 export class HashMap {
-    data = new Array(3);
+    data = new Array(7);
     capacity = 0;
+    loadFactor = 0.75;
 
     hash(key, tableSize) {
         let hashCode = 0;
@@ -12,7 +13,7 @@ export class HashMap {
     }
 
     resize() {
-        const newData = new Array(this.data.length * 2);
+        const newData = new Array(this.data.length * 2 - 1);
         this.data.forEach((item) => {
             if (item) {
                 item.forEach(([key, value]) => {
@@ -25,13 +26,13 @@ export class HashMap {
                 });
             }
         });
-        this.table = newData;
+        this.data = newData;
     }
 
     set(key, value) {
         this.capacity++;
-        const loadFactor = this.capacity / this.data.length;
-        if (loadFactor > 0.75) {
+        const loadFactor = 0.75;
+        if (this.capacity / this.data.length > loadFactor) {
             this.resize();
         }
 
@@ -50,5 +51,74 @@ export class HashMap {
         }
 
         return this.data[idx].find((x) => x[0] === key)[1];
+    }
+
+    has(key) {
+        const idx = this.hash(key, this.data.length);
+        if (this.data[idx]) {
+            if (this.data[idx].find((x) => x[0] === key)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    remove(key) {
+        const idx = this.hash(key, this.data.length);
+        const element = this.data[idx].find((x) => x[0] === key);
+        const elementIdx = this.data[idx].indexOf(element);
+        if (this.data[idx]) {
+            if (element) {
+                this.data[idx].splice(elementIdx, 1);
+                this.capacity--;
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    length() {
+        return this.data.length;
+    }
+
+    clear() {
+        this.data.splice(0, this.data.length);
+    }
+
+    keys() {
+        const result = [];
+        this.data.forEach((item) => {
+            item.forEach(([key, value]) => {
+                result.push(key);
+            });
+        });
+
+        return result;
+    }
+
+    values() {
+        const result = [];
+        this.data.forEach((item) => {
+            item.forEach(([key, value]) => {
+                result.push(value);
+            });
+        });
+
+        return result;
+    }
+
+    entries() {
+        const result = [];
+        this.data.forEach((item) => {
+            item.forEach(([key, value]) => {
+                result.push([key, value]);
+            });
+        });
+
+        return result;
     }
 }
--- src/index.js
+++ src/index.js
@@ -8,15 +8,30 @@ test.set("carrot", "orange");
 test.set("dog", "brown");
 test.set("elephant", "gray");
 test.set("frog", "green");
+// after this point the whole array gets resized
 test.set("grape", "purple");
 test.set("hat", "black");
 test.set("ice cream", "white");
 test.set("jacket", "blue");
+// array gets resized again
 test.set("kite", "pink");
 test.set("lion", "golden");
+test.set("moon", "silver");
 
-// console.log(test);
+// console.log(test.data);
 
-console.log(test.get("jacket"));
-console.log(test.get("grape"));
-console.log(test.get("lion"));
+// console.log(test.get("jacket"));
+
+// console.log(test.has("apple"));
+
+// console.log(test.remove("dog"));
+
+// console.log(test.length());
+
+// test.clear();
+
+// console.log(test.keys());
+
+// console.log(test.values());
+
+// console.log(test.entries());
